986b6962804df91a6ae659a2e230ca1c4013630a
YSn0630 <rkrtkdgj@gmail.com>
2025-10-15 12:23:11 +0900

    "업.뎃.완. 공백은 금기."

--- Lunch_Choice.py
+++ Lunch_Choice.py
@@ -6,14 +6,17 @@ ll = []
 le = ''
 
 while (le != '끝'):
-    le = input("뭐.먹.싶? > ")
+    le = input("뭐.먹.싶?(그.싶.끝.입.) > ")
     if le != '끝':
         ll.append(le)
 
+lc = []
 lc = random.sample(ll,1)
-
 print("오늘의 메뉴는...")
 
 time.sleep(3)
 
-print(lc)
+if "" not in lc:
+    print(*lc)
+else:
+    print("굶.\n공백, 괘씸.")
